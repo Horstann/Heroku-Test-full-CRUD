454920712328d8cc2b21be36b768f5fd554cff3f
Horstann <horstannho@gmail.com>
2022-08-09 12:01:01 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+/*
+npm init
+npm i express ...
+*/
+if (process.env.NODE_ENV !== "production") { // if === roduction, it's on heroku
+    require('dotenv').config();
+}
+
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const Product = require('./models/product.js');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/farmStand';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MONGO CONNECTION ERROR"));
+db.once("open", () => {
+    console.log("DATABASE CONNECTED");
+})
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true})); // so that req.body isn't undefined
+app.use(methodOverride('_method'));
+
+
+app.get('/products', async (req, res) => {
+    // 1st check if there's a query string
+    const {category} = req.query;
+    if (category){
+        const products = await Product.find({category})
+        res.render('products/index', {products, category})
+    }else{
+        // if no query string, set category to 'All'
+        const products = await Product.find({});
+        res.render('products/index', {products, category: 'All'});
+    }
+})
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+app.get('/products/new', async(req, res) => {
+    res.render('products/new', {categories});
+})
+
+app.post('/products', async(req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}`);
+})
+
+app.get('/products/:id', async(req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log(product);
+    res.render('products/show', {product});
+})
+
+app.get('/products/:id/edit', async(req, res) => {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    console.log('HELLO '+product.category);
+    res.render('products/edit', {product, categories});
+})
+
+app.put('/products/:id', async(req, res) => {
+    console.log(req.body);
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async(req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`SERVING ON PORT ${port}`);
+})
+
+
---
+++ seeds.js
@@ -0,0 +1,73 @@
+// this file is not connected to the app
+// anytime it runs, it seeds the database
+
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+// INSERT ONE
+
+const p = new Product({
+    name: 'Ruby Grapefruit',
+    price: 1.99,
+    category: 'fruit'
+})
+p.save()
+    .then(p => {
+        console.log(p)
+    })
+    .catch(e => {
+        console.log(e)
+    })
+
+    mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('MONGO CONNECTION OPEN!');
+    })
+    .catch(err => {
+        console.log('OH NO MONGO CONNECTION ERROR!');
+        console.log(err);
+    })
+
+
+// INSERT MANY
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
+]
+// if any validations fail, nothing is inserted!
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
+
+
+
+
+
